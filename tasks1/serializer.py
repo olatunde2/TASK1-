c607c0e94760abcff07b832cfa470165bcc1a40e
olatunde2 <133256292+olatunde2@users.noreply.github.com>
2023-09-09 00:50:50 +0300

    Update serializer.py

--- tasks1/serializer.py
+++ tasks1/serializer.py
@@ -1,4 +1,4 @@
 from rest_framework import serializers
 
 class Datetimeserializer(serializers.Serializer):
-    current_date = serializers.DateField(format="2023-09-08T%H:%M:%SZ")
+    current_date = serializers.DateField(format="2023-09-08T%H:%M:%S")
