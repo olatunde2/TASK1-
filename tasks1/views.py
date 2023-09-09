8066378e2b6aa54322ccad95963b81299ab0702d
olatunde2 <133256292+olatunde2@users.noreply.github.com>
2023-09-09 10:51:46 +0300

    Update views.py

--- tasks1/views.py
+++ tasks1/views.py
@@ -8,7 +8,7 @@ from django.http import JsonResponse
 def CustomAPI(request):
     current_datetime = datetime.now()
     formatted_date = current_datetime.strftime("%Y-%m-%d")
-    formatted_time = current_datetime.strftime("%H:%M:%S")
+    formatted_time = current_datetime.strftime("%H:%M:%SZ")
     response_data={
         'current_date':formatted_date,
         'current_time': formatted_time
@@ -21,8 +21,15 @@ class CustomAPI(APIView):
     def get(self, request):
         param_value = request.query_params.get("slack_name=olatunde&track=backend")
         current_datetime = datetime.now()
-        utc_time = Datetimeserializer({'current_date: current_datetime.time()})
+        current = Datetimeserializer({
+             "slack_name": "olatunde", 
+             "current_day":"Saturday",
+             "utc_time": current_datetime.time(),
+             "track": "backend", 
+             "github_file_url": "https://github.com/olatunde2/TASK1-/tree/main/tasks1", 
+             "github_repo_url": "https://github.com/olatunde/task1.git", 
+             "status_code": 200
+            }).data
 
-        response = { "slack_name": "olatunde", "current_day": "Friday", "utc_time": utc_time.data, "track": "backend", "github_file_url": "https://github.com/olatunde/TASK1-.git", "github_repo_url": "https://github.com/olatunde2/TASK1-/blob/main/tasks1/views.py", "status_code": 200}
    
-        return Response(response)
+        return Response(current)
