a17fa0c7cd9ea0cc5ef85c538d31692475a710e3
olatunde2 <133256292+olatunde2@users.noreply.github.com>
2023-09-09 10:50:40 +0300

    Update serializer.py

--- tasks1/serializer.py
+++ tasks1/serializer.py
@@ -1,4 +1,10 @@
 from rest_framework import serializers
 
 class Datetimeserializer(serializers.Serializer):
-    current_date = serializers.DateField(format="2023-09-08T%H:%M:%SZ")
+    slack_name = serializers.CharField()
+    current_day = serializers.DateField()
+    utc_time = serializers.DateTimeField(format="2023-09-09T%H:%M:%SZ")
+    track = serializers.CharField()
+    github_file_url = serializers.CharField() 
+    github_repo_url = serializers.CharField() 
+    status_code = serializers.IntegerField()
